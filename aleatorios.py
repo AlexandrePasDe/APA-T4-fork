4dc766f8ae8da2b94336c46a094b2644118a2d2b
Alex Pascual <alexandre.pascual@estudiantat.upc.edu>
2026-06-12 22:55:02 +0200

    Add files via upload

---
+++ aleatorios.py
@@ -0,0 +1,69 @@
+"""
+Cuarta tarea APA - Generación de números aleatorios
+Alumno: Alexandre Pascual Deza
+"""
+
+class Aleat:
+    """
+    >>> rand = Aleat(m=32, a=9, c=13, x0=11)
+    >>> for _ in range(4):
+    ...     print(next(rand))
+    ...
+    16
+    29
+    18
+    15
+
+    >>> rand(29)
+    >>> for _ in range(4):
+    ...     print(next(rand))
+    ...
+    18
+    15
+    20
+    1
+    """
+
+    def __init__(self, *, m=2**48, a=25214903917, c=11, x0=1212121):
+        self.m = m
+        self.a = a
+        self.c = c
+        self.x = x0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.x = (self.a * self.x + self.c) % self.m
+        return self.x
+
+    def __call__(self, seed):
+        self.x = seed
+
+
+def aleat(m=2**48, a=25214903917, c=11, x0=1212121):
+    """
+    >>> rand = aleat(m=64, a=5, c=46, x0=36)
+    >>> for _ in range(4):
+    ...     print(next(rand))
+    ...
+    34
+    24
+    38
+    44
+
+    >>> rand.send(24)
+    38
+    >>> for _ in range(4):
+    ...     print(next(rand))
+    ...
+    44
+    10
+    32
+    14
+    """
+    x = x0
+    while True:
+        new_seed = yield (x := (a * x + c) % m)
+        if new_seed is not None:
+            x = new_seed
